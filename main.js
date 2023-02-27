180f50ca5b7108d9f98ca36e9d5f7db7c5354dd1
SonamMalik17 <maliksonam0301@gmail.com>
2023-02-28 00:37:07 +0530

    Final touch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(".stopwatch-btn").click(function(){
+$(".stopwatch-btn").click(function () {
     // Hide all other wrappers
     $(".outer-wrapper > div").slideUp();
     // Show stopwatch wrapper
@@ -8,46 +8,46 @@ $(".stopwatch-btn").click(function(){
 });
 
 
-$(".back-btn").click(function(){
+$(".back-btn").click(function () {
     // Hide all other wrappers
     $(".outer-wrapper > div").slideUp();
     // Show clock wrapper
     $(".clock").slideDown();
     // update type text
-    $(".type").html("Stopwatch");
+    $(".type").html("Clock");
 });
 
 
-$(".timer-btn").click(function(){
+$(".timer-btn").click(function () {
     // Hide all other wrappers
     $(".outer-wrapper > div").slideUp();
     // Show timer wrapper
     $(".timer").slideDown();
     // update type text
-    $(".type").html("Stopwatch");
+    $(".type").html("Timer");
 });
 
 
-const addTrailingZero = (num) =>{
-    return num < 10 ? "0" + num : num; 
+const addTrailingZero = (num) => {
+    return num < 10 ? "0" + num : num;
 };
 
 
-const updateTime=()=>{
-    const time=new Date();
-    let hours=time.getHours();
-    let minutes=time.getMinutes();
-    let seconds=time.getSeconds();
-    let ampm=hours>=12? "PM" : "AM";
-    let otherampm=hours>=12? "AM" : "PM";
+const updateTime = () => {
+    const time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    let otherampm = hours >= 12 ? "AM" : "PM";
 
     // converting 24 hours to 12
     hours = hours % 12 || 12;
 
     // add trailing zeros if less than 10
-    hours=addTrailingZero(hours);
-    minutes=addTrailingZero(minutes);
-    seconds=addTrailingZero(seconds);
+    hours = addTrailingZero(hours);
+    minutes = addTrailingZero(minutes);
+    seconds = addTrailingZero(seconds);
 
     $("#hour").html(hours);
     $("#min").html(minutes);
@@ -66,33 +66,34 @@ setInterval(updateTime, 1000);
 
 
 // Stopwatch
-let stopwatchHours=0,
-stopwatchMinutes=0,
-stopwatchSeconds=0,
-stopwatchMiliSeconds=0,
-stopwatchRunning=false,
-laps=0,
-stopwatchInterval;
+let stopwatchHours = 0,
+    stopwatchMinutes = 0,
+    stopwatchSeconds = 0,
+    stopwatchMiliSeconds = 0,
+    stopwatchRunning = false,
+    laps = 0,
+    stopwatchInterval;
 
 
-const stopwatch = () =>{
+const stopwatch = () => {
     // Increase milisecond by one
     stopwatchMiliSeconds++;
 
-    if(stopwatchMiliSeconds==100){
+    // Although it is actually centiseconds
+    if (stopwatchMiliSeconds == 100) {
         // if stopwatch milisecond equals 100 increase one second and set ms 0
         stopwatchSeconds++;
-        stopwatchMiliSeconds=0;
+        stopwatchMiliSeconds = 0;
     }
-    if(stopwatchSeconds==60){
+    if (stopwatchSeconds == 60) {
         // same with minutes
         stopwatchMinutes++;
-        stopwatchSeconds=0;
+        stopwatchSeconds = 0;
     }
-    if(stopwatchMinutes==60){
+    if (stopwatchMinutes == 60) {
         // same with hours
         stopwatchHours++;
-        stopwatchMinutes=0;
+        stopwatchMinutes = 0;
     }
     // show values on document
     $('#stopwatch-hour').html(addTrailingZero(stopwatchHours));
@@ -103,32 +104,32 @@ const stopwatch = () =>{
 
 
 // function to start stopwatch
-const startStopwatch = () =>{
-    if(!stopwatchRunning){
+const startStopwatch = () => {
+    if (!stopwatchRunning) {
         // If stopwatch not already running
-        stopwatchInterval=setInterval(stopwatch,10);
-        stopwatchRunning=true;
+        stopwatchInterval = setInterval(stopwatch, 10);
+        stopwatchRunning = true;
     }
 };
 
 
 // function to stop stopwatch
-const stopStopwatch = () =>{
+const stopStopwatch = () => {
     clearInterval(stopStopwatch);
-    stopwatchRunning=false;
+    stopwatchRunning = false;
 };
 
 
 // reset stopwatch function
-const resetStopwatch = () =>{
+const resetStopwatch = () => {
     // clear interval and set all values to default
     clearInterval(stopwatchInterval);
-    stopwatchHours=0;
-    stopwatchMinutes=0;
-    stopwatchSeconds=0;
-    stopwatchMiliSeconds=0;
-    stopwatchRunning=false;
-    laps=0;
+    stopwatchHours = 0;
+    stopwatchMinutes = 0;
+    stopwatchSeconds = 0;
+    stopwatchMiliSeconds = 0;
+    stopwatchRunning = false;
+    laps = 0;
 
     // update values on document to 00
     $('#stopwatch-hour').html("00");
@@ -140,7 +141,7 @@ const resetStopwatch = () =>{
 
 
 // Start stopwatch on start button
-$(".start-stopwatch").click(function (){
+$(".start-stopwatch").click(function () {
     startStopwatch();
     // hide start button show lap button
     $(".start-stopwatch").hide();
@@ -148,14 +149,14 @@ $(".start-stopwatch").click(function (){
 });
 
 
-$(".reset-stopwatch").click(function(){
+$(".reset-stopwatch").click(function () {
     resetStopwatch();
     $(".start-stopwatch").show();
     $(".lap-stopwatch").hide();
 });
 
 
-$(".lap-stopwatch").click(function(){
+$(".lap-stopwatch").click(function () {
     // on lap button click
     laps++;
     // remove active class
@@ -172,27 +173,27 @@ $(".lap-stopwatch").click(function(){
 
 
 // Timer
-let time=0,
-timerHours=0,
-timerMinutes=0,
-timerSeconds=0,
-timerMiliseconds=0,
-timerInterval;
+let time = 0,
+    timerHours = 0,
+    timerMinutes = 0,
+    timerSeconds = 0,
+    timerMiliseconds = 0,
+    timerInterval;
 
 
-const getTime = () =>{
-    time=prompt("Enter time in minutes");
+const getTime = () => {
+    time = prompt("Enter time in minutes");
     // convert time to seconds
-    time=time*60;
+    time = time * 60;
     // update time defaults
     setTime();
 };
 
 
-const setTime = () =>{
-    timerHours = Math.floor(time/3600);
-    timerMinutes = Math.floor((time & 3600)/60);
-    timerSeconds=Math.floor(time%60);
+const setTime = () => {
+    timerHours = Math.floor(time / 3600);
+    timerMinutes = Math.floor((time & 3600) / 60);
+    timerSeconds = Math.floor(time % 60);
     // Show user entered time on document
     $("#timer-hour").html(addTrailingZero(timerHours));
     $("#timer-min").html(addTrailingZero(timerMinutes));
@@ -203,16 +204,16 @@ const setTime = () =>{
 
 const timer = () => {
     timerMiliseconds--;
-    if(timerMiliseconds==-1){
-        timerMiliseconds=99;
+    if (timerMiliseconds == -1) {
+        timerMiliseconds = 99;
         timerSeconds--;
     }
-    if(timerSeconds==-1){
-        timerSeconds=59;
+    if (timerSeconds == -1) {
+        timerSeconds = 59;
         timerMinutes--;
     }
-    if(timerMinutes==-1){
-        timerMinutes=59;
+    if (timerMinutes == -1) {
+        timerMinutes = 59;
         timerHours--;
     }
     // update time
@@ -227,15 +228,15 @@ const timer = () => {
 
 const startTimer = () => {
     // before starting clock if valid time given
-    if(
-        (timerHours==0) && (timerMinutes==0) && timerSeconds==0 && timerMiliseconds==0
-    ){
+    if (
+        (timerHours == 0) && (timerMinutes == 0) && timerSeconds == 0 && timerMiliseconds == 0
+    ) {
         // if all values are zero get time
         getTime();
     }
-    else{
+    else {
         // start timer
-        timerInterval=setInterval(timer,10);
+        timerInterval = setInterval(timer, 10);
         $('.start-timer').hide();
         $('.stop-timer').show();
     }
@@ -251,33 +252,33 @@ const stopTimer = () => {
 
 const resetTimer = () => {
     stopTimer();
-    time=0;
+    time = 0;
     setTime();
 };
 
 
 // check if time remaining is zero
 const timeUp = () => {
-    if(
-        (timerHours==0) && (timerMinutes==0) && timerSeconds==0 && timerMiliseconds==0
-    ){
+    if (
+        (timerHours == 0) && (timerMinutes == 0) && timerSeconds == 0 && timerMiliseconds == 0
+    ) {
         stopTimer();
         alert("Time's up");
     }
 };
 
 
-$(".start-timer").click(function(){
+$(".start-timer").click(function () {
     startTimer();
 });
 
 
-$(".stop-timer").click(function(){
+$(".stop-timer").click(function () {
     stopTimer();
 });
 
 
-$(".reset-timer").click(function(){
+$(".reset-timer").click(function () {
     resetTimer();
 });
 
